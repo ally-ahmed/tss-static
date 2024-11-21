67569f247f24735420d72054832d375d2821fc03
Ahmed <norbixin@live.com>
2024-11-21 16:20:39 +0300

    use VITE BASE

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -7,7 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   if (import.meta.env.VITE_APP_BASE_URL)
     return `https://${import.meta.env.VITE_APP_BASE_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
