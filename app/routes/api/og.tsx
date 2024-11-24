0920568e581ec630447f0c37cd9a696797e5795d
Ahmed <norbixin@live.com>
2024-11-24 09:46:17 +0300

    remove @vercel/og form og route

--- app/routes/api/og.tsx
+++ app/routes/api/og.tsx
@@ -1,33 +1,34 @@
 import { createAPIFileRoute } from "@tanstack/start/api";
-import { ImageResponse } from "@vercel/og";
+// import { ImageResponse } from "@vercel/og";
 
 export const Route = createAPIFileRoute("/api/og")({
   GET: ({ request }) => {
     const url = new URL(request.url);
     const heading = url.searchParams.get("heading");
+    return new Response(heading);
 
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            fontSize: 40,
-            color: "black",
-            background: "white",
-            width: "100%",
-            height: "100%",
-            padding: "50px 200px",
-            textAlign: "center",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {heading ? `🤯 ${heading}` : "👋 Hello"}
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      },
-    );
+    // return new ImageResponse(
+    //   (
+    //     <div
+    //       style={{
+    //         fontSize: 40,
+    //         color: "black",
+    //         background: "white",
+    //         width: "100%",
+    //         height: "100%",
+    //         padding: "50px 200px",
+    //         textAlign: "center",
+    //         justifyContent: "center",
+    //         alignItems: "center",
+    //       }}
+    //     >
+    //       {heading ? `🤯 ${heading}` : "👋 Hello"}
+    //     </div>
+    //   ),
+    //   {
+    //     width: 1200,
+    //     height: 630,
+    //   },
+    // );
   },
 });
